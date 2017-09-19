341b50c53f2a27353a51b8a1b01a085375252727
Shawn Rider <shawn@shawnrider.com>
2017-09-19 10:17:12 -0700

    Initial commit.
    Change-Id: I93fd487d9185dfaa93a7b3619c932e102fefd64a

---
+++ src/components/Hello.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+    <textarea :value="input" @input="update"></textarea>
+    <div v-html="compiledMarkdown"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      input: '# hello'
+    }
+  },
+  computed: {
+    compiledMarkdown: function () {
+      return marked(this.input, { sanitize: true })
+    }
+  },
+  methods: {
+    update: _.debounce(function (e) {
+      this.input = e.target.value
+    }, 300)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello {
+  margin: 0;
+  height: 100%;
+  font-family: 'Helvetica Neue', Arial, sans-serif;
+  color: #333;
+}
+
+textarea, .hello div {
+  display: inline-block;
+  width: 49%;
+  height: 75vh;
+  vertical-align: top;
+  box-sizing: border-box;
+  padding: 0 20px;
+  text-align: left;
+}
+
+textarea {
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f6f6f6;
+  font-size: 14px;
+  font-family: 'Monaco', courier, monospace;
+  padding: 20px;
+}
+
+code {
+  color: #f66;
+}
+</style>
